50797b9e846b15f2d9659c5c9940531325612999
nice-kun <skyamaterasu@outlook.com>
2015-09-10 15:03:47 +1000

    port from 8080 -> 80

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var PORT = process.env.OPENSHIFT_INTERNAL_PORT || 80;
 var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1';
 
 var express = require('express');
